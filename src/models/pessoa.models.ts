0305a6389a323c4b8321399661b725bec1cd4b79
Paulo André <andrepaulos.2001@gmail.com>
2026-06-10 13:42:14 -0300

    feat: add typeORM decorators to Pessoa model

--- src/models/pessoa.models.ts
+++ src/models/pessoa.models.ts
@@ -1,19 +1,55 @@
+import { Column, Entity, PrimaryColumn, PrimaryGeneratedColumn } from "typeorm";
 import { Endereco } from "./endereco.model.js";
 
 export class Pessoa {
     
+    @PrimaryGeneratedColumn("increment")
+    public id?: bigint;
+
+    @Column({type: "varchar", nullable: false})
+    public uuid?: string;
+
+    @Column({ type: "varchar", length: 255 })
+    public nome?: string;
+
+    @Column({ type: "varchar", length: 11 })
+    public cpf?: string;
+
+    @Column({ type: "varchar" })
+    public email?: string;
+
+    @Column({ type: "varchar"})
+    public senhaHash?: string;
+
+    // Essa expressão abaixo faz essa Entity aqui criar colunas usando o mapeamento de colunas da CLasse Endereco.
+    // então a classe Pessoa faria colunas do tipo: EnderecoNumero, EndereroPais, etc...
+    @Column(() => Endereco)
+    public endereco?: Endereco;
+
     constructor(
-        public nome: string,
-        public cpf: string,
-        public email: string,
-        public senhaHash: string,
-        public id?: bigint,
-        public uuid?: string,
-        public endereco?: Endereco
+        nome?: string,
+        cpf?: string,
+        email?: string,
+        senhaHash?: string,
+        id?: bigint,
+        uuid?: string,
+        endereco?: Endereco
     ){ 
-        if (!this.validaCpf(this.cpf)) throw new Error("Não é possível criar Pessoa: CPF inválido");
-        if (!this.email.includes("@")) throw new Error("Não é possível criar Pessoa: Email inválido");
-        if (this.senhaHash.trim().length === 0) throw new Error("Não é possível criar Pessoa: Senha vazia");
+        if(nome && cpf && email && senhaHash){
+            if (!this.validaCpf(cpf)) throw new Error("Não é possível criar Pessoa: CPF inválido");
+            if (!email.includes("@")) throw new Error("Não é possível criar Pessoa: Email inválido");
+            if (senhaHash.trim().length === 0) throw new Error("Não é possível criar Pessoa: Senha vazia");
+            
+            this.nome = nome;
+            this.cpf = cpf;
+            this.email = email;
+            this.senhaHash = senhaHash;
+            
+            this.id =  id;
+            this.uuid =  uuid;
+            this.endereco =  endereco;
+        }
+
     }
 
     private validaCpf(cpf: string): boolean {
