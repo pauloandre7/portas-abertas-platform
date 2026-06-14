920bc7079c972e7eeb1d1a7f5e52175947fa0404
Paulo André <andrepaulos.2001@gmail.com>
2026-06-14 02:47:17 -0300

    feat: add findByUuid an update delete method param in InstituicaoRepository

--- src/repositories/iinstituicao.repositories.ts
+++ src/repositories/iinstituicao.repositories.ts
@@ -2,10 +2,11 @@ import type { Instituicao } from "../models/instituicao.model.js"
 
 export interface IInstituicaoRepository {
 
-    delete(instituicao: Instituicao)    : Promise<boolean>;
+    delete(uuid: string)                : Promise<boolean>;
     create(instituicao: Instituicao)    : Promise<boolean>;
     update(instituicao: Instituicao)    : Promise<boolean>;
     findById(id: bigint)                : Promise<Instituicao | null>;
+    findByUuid(uuid: string)            : Promise<Instituicao | null>;
     findAll()                           : Promise<Instituicao[] | null>;
     findByCidade(cidade : string)       : Promise<Instituicao[] | null>;
     findByServicos(servicos: string[])  : Promise<Instituicao[] | null>;
