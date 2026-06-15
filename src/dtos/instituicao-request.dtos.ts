756f2fce155e6c381aa422cfa924278e45d7977a
Paulo André <andrepaulos.2001@gmail.com>
2026-06-15 19:31:12 -0300

    feat: add status field to InstituicaoRequest DTO

--- src/dtos/instituicao-request.dtos.ts
+++ src/dtos/instituicao-request.dtos.ts
@@ -1,5 +1,6 @@
 import type { ContatoDto } from "./contato.dtos.js";
 import type { EnderecoDto } from "./endereco.dtos.js";
+import type { StatusInstituicao } from '../models/instituicao-status-enum.models.js';
 
 export class InstituicaoRequest {
 
@@ -8,6 +9,7 @@ export class InstituicaoRequest {
         public nome: string,
         public cnpj: string,
         public descricao: string,
+        public status: StatusInstituicao,
         public servicos: string[],
         public contato: ContatoDto,
         public endereco: EnderecoDto
