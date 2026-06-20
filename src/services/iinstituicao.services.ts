284a7334d0d3570383b6ddb84811790cfd82f567
pauloandre7 <andrepaulos.2001@gmail.com>
2026-06-19 23:44:00 -0300

    feat: adds method to find by uuid
    this commits adds findByUuid at the contract and refactor delete method

--- src/services/iinstituicao.services.ts
+++ src/services/iinstituicao.services.ts
@@ -1,5 +1,4 @@
 import type { InstituicaoCreateRequest } from "../dtos/instituicao-create-request.dtos.js";
-import type { InstituicaoDeleteRequest } from "../dtos/instituicao-delete-request.dtos.js";
 import type { InstituicaoRequest } from "../dtos/instituicao-request.dtos.js";
 
 export interface IInstituicaoService {
@@ -7,9 +6,10 @@ export interface IInstituicaoService {
     cadastrarInstituicao(cadastrarRequest: InstituicaoCreateRequest): Promise<InstituicaoRequest>;
     // Dá pra usar esse modificar para trocar status também
     modificarInstituicao(updateRequest: InstituicaoRequest): Promise<InstituicaoRequest>;
-    excluirInstituicao(deleteRequest: InstituicaoDeleteRequest): Promise<boolean>;
+    excluirInstituicao(uuid: string): Promise<boolean>;
     listarTodasInstituicoes(): Promise<InstituicaoRequest[]>;
     listarPorServicos(servicos: string[]) : Promise<InstituicaoRequest[]>;
     listarPorLocalizacao(cidade: string): Promise<InstituicaoRequest[]>;
     buscarPorNome(nome: string): Promise<InstituicaoRequest>;
+    buscaPorUuid(uuid: string) : Promise<InstituicaoRequest>;
 }
